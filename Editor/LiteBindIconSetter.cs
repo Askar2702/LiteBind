ca7ccb84a477d71b4ce609d7064799498697fabc
Askar2702 <56299054+Askar2702@users.noreply.github.com>
2025-03-27 13:43:26 +0500

    исправление получение иконки

--- Editor/LiteBindIconSetter.cs
+++ Editor/LiteBindIconSetter.cs
@@ -1,6 +1,5 @@
-using System;
-using System.Drawing;
 using System.IO;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
@@ -9,20 +8,37 @@ public static class LiteBindIconSetter
 {
     static Texture2D _icon;
     private const string targetNamespace = "LiteBindDI";
-    // Ключевые слова для поиска не-классовых типов
     private static readonly string[] typeKeywords = { "interface", "struct", "enum" };
 
     static LiteBindIconSetter()
     {
-        _icon = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/LiteBind/Editor/LiteBindDI_Icon.png");
+        string iconPath = GetIconPath();
+        if (string.IsNullOrEmpty(iconPath)) return;
+
+        _icon = AssetDatabase.LoadAssetAtPath<Texture2D>(iconPath);
         if (_icon == null) return;
 
         EditorApplication.delayCall += AssignIcons;
     }
 
+    private static string GetIconPath()
+    {
+        // Находим сам скрипт этого класса
+        string[] guids = AssetDatabase.FindAssets("LiteBindIconSetter t:Script");
+        if (guids.Length == 0) return null;
+
+        string scriptPath = AssetDatabase.GUIDToAssetPath(guids[0]);
+        if (string.IsNullOrEmpty(scriptPath)) return null;
+
+        string basePath = Path.GetDirectoryName(scriptPath); // путь до папки Editor
+        string iconPath = Path.Combine(basePath, "LiteBindDI_Icon.png");
+        return iconPath.Replace("\\", "/"); // на всякий случай
+    }
+
     private static void AssignIcons()
     {
-        string[] guids = AssetDatabase.FindAssets("t:MonoScript", new[] { "Assets/LiteBind" });
+        string[] guids = AssetDatabase.FindAssets("t:MonoScript", new[] { "Assets", "Packages" });
+
         foreach (string guid in guids)
         {
             string scriptPath = AssetDatabase.GUIDToAssetPath(guid);
@@ -31,28 +47,18 @@ public static class LiteBindIconSetter
                 continue;
 
             bool matchesNamespace = false;
-            // Попытка получить класс из скрипта
             var scriptClass = script.GetClass();
+
             if (scriptClass != null)
             {
-                // Если класс найден, проверяем его пространство имён
                 matchesNamespace = scriptClass.Namespace == targetNamespace;
             }
             else
             {
-                // Если класс не найден, возможно, это интерфейс, структура или перечисление.
-                // Считываем содержимое файла и ищем нужные ключевые слова и пространство имён.
                 string fileText = File.ReadAllText(scriptPath);
                 if (fileText.Contains("namespace " + targetNamespace))
                 {
-                    foreach (var keyword in typeKeywords)
-                    {
-                        if (fileText.Contains(keyword))
-                        {
-                            matchesNamespace = true;
-                            break;
-                        }
-                    }
+                    matchesNamespace = typeKeywords.Any(fileText.Contains);
                 }
             }
 
